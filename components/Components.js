1362c124f6f02e85bc248d3a0c2e231dafe4e6d3
Anshul Kamath <anshulkam@gmail.com>
2020-07-26 17:51:06 -0400

    Added card and workout card

--- components/Components.js
+++ components/Components.js
@@ -3,3 +3,5 @@ export { default as ButtonRow } from './ButtonRow.js';
 export { default as CreateSingleStringForm } from './SingleStringForm';
 export { default as CreateTwoOptionForm } from './TwoOptionForm';
 export { default as ScrollWheel } from './ScrollWheel';
+export { default as Card } from './Cards/Card';
+export { default as WorkoutCard } from './Cards/WorkoutCard';
--- utilities/Colors.js
+++ utilities/Colors.js
@@ -1,7 +1,9 @@
 export default {
     Red: '#FA897B',
+    Green: "#86E3CE",
+    Purple: "#CCABDB",
     White: '#FFFFFF',
     Black: '#000000',
     Grey: '#D6D6D6',
-    LightGrey: '#EEEEEE',
+    LightGrey: '#EDEDED',
 }
